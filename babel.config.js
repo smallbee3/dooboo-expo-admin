24f6414d8bd757d9cbe689892a512ba75b2d1c50
clark <smallbee3@gmail.com>
2020-07-20 16:50:41 +0900

    Add babel.config.js for relay

--- babel.config.js
+++ babel.config.js
@@ -2,17 +2,9 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo', '@babel/preset-typescript'],
-    // sourceMaps: "inline",
-    // plugins: [
-    //   '@babel/transform-react-jsx-source',
-    //   [
-    //     "@babel/plugin-transform-runtime",
-    //     {
-    //       "helpers": true,
-    //       "regenerator": false
-    //     }
-    //   ],
-    //   "@babel/proposal-object-rest-spread"
-    // ],
+    plugins: [
+      ['relay', { artifactDirectory: './src/__generated__' }],
+      'macros',
+    ],
   };
 };

aa4e5994756af4001c82f91fc041825fa90ce137
clark <smallbee3@gmail.com>
2020-07-20 20:24:25 +0900

    Update test files

--- test/testUtils.tsx
+++ test/testUtils.tsx
@@ -1,28 +1,42 @@
 import 'react-native';
 
 import React, { ReactElement } from 'react';
+import { RelayMockEnvironment, createMockEnvironment } from 'relay-test-utils';
+import { ThemeProvider, ThemeType } from '@dooboo-ui/theme';
+import { dark, light } from '../src/theme';
 
-import RootProvider from '../src/providers';
-import { ThemeType } from '../src/providers/ThemeProvider';
+import { AppProvider } from '../src/providers/AppProvider';
+import { RelayEnvironmentProvider } from 'react-relay/hooks';
+
+export const environment: RelayMockEnvironment = createMockEnvironment();
 
 export const createTestElement = (
   child: ReactElement,
-  themeType?: ThemeType,
-): ReactElement => (
-  <RootProvider initialThemeType={themeType}>{child}</RootProvider>
-);
+  themeType: ThemeType = ThemeType.LIGHT,
+): ReactElement => {
+  return (
+    <RelayEnvironmentProvider environment={environment}>
+      <ThemeProvider
+        customTheme={{
+          light,
+          dark,
+        }}
+        initialThemeType={themeType}
+      >
+        <AppProvider>{child}</AppProvider>
+      </ThemeProvider>
+    </RelayEnvironmentProvider>
+  );
+};
 
 export const createTestProps = (
-  obj?: object,
-  moreScreenProps?: object,
-): object | unknown | any => ({
+  obj: Record<string, unknown> = {},
+): Record<string, unknown> | unknown | any => ({
   navigation: {
     navigate: jest.fn(),
     goBack: jest.fn(),
-  },
-  screenProps: {
-    changeThemeType: jest.fn(),
-    ...moreScreenProps,
+    replace: jest.fn(),
+    setOptions: jest.fn(),
   },
   ...obj,
 });
